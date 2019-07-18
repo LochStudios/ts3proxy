b906bad3bcd303b84d645cad66612d81beb34539
LochStudios <lachlan@lochstudios.com>
2019-07-18 22:32:25 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
-pip3 install .
-cp -n config.example.yml config.yml
+sudo apt-get update
+sudo apt-get install python3 python3-yaml -y
+sudo python -m pip install --upgrade pip
+sudo pip3 install .
 "${EDITOR:-nano}" config.yml
 echo "Successfully installed and configured TS3Proxy. Start it using ts3proxy"
